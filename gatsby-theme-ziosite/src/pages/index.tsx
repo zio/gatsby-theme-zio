4b61428f073c136b113071ec32881100d5ffd526
Andreas Gies <andreas@wayofquality.de>
2022-06-20 18:11:30 +0200

    Try responsive tailwind for home screen

--- gatsby-theme-ziosite/src/pages/index.tsx
+++ gatsby-theme-ziosite/src/pages/index.tsx
@@ -12,7 +12,7 @@ const IndexPage = () => {
         <div className="container flex flex-row max-w-full w-full relative bg-black place-content-center">
           <div className="relative place-content-center">
             <div className="absolute left-0 top-1/3 w-full z-10 flex flex-col">
-              <span className="text-primary-50 mt-10 mx-auto">{tagLine}</span>
+              <span className="text-primary-50 mt-10 mx-auto text-center h-0 invisible md:visible md:h-auto">{tagLine}</span>
               <Link to="/foo/2-0-0/docs" className="text-primary-50 mt-2 m-auto p-2 border-solid border-primary-50 border rounded-lg hover:bg-primary-600">Get Started</Link>
             </div>
             <StaticImage 
