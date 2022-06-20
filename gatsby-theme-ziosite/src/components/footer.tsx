ce1f99926c18c9dd9cde9b93f236cc54c739ddd4
Andreas Gies <andreas@wayofquality.de>
2022-06-20 21:08:18 +0200

    Responsive Footer

--- gatsby-theme-ziosite/src/components/footer.tsx
+++ gatsby-theme-ziosite/src/components/footer.tsx
@@ -7,7 +7,7 @@ const year = new Date().getFullYear()
 const Footer = () => { 
   return(
     <div className="container max-w-full w-full bg-primary-700 text-primary-50 flex flex-col">
-      <div className="flex flex-row justify-center p-4">
+      <div className="flex flex-col md:flex-row justify-center p-4">
         <img className="flex-initial mx-4" src="/img/navbar_brand.png" alt="zio" />
         <Social title="Github">
           <a href="https://github.com/zio/zio" className="m-auto">
