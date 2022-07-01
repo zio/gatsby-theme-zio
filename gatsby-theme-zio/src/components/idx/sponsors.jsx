1f2e1f428371d8b6fb8841a762f36dfd82995232
Andreas Gies <andreas@wayofquality.de>
2022-07-01 21:24:57 +0200

    Update components

--- gatsby-theme-zio/src/components/idx/sponsors.jsx
+++ gatsby-theme-zio/src/components/idx/sponsors.jsx
@@ -2,19 +2,19 @@ import * as React from "react"
 
 const sponsors = [
   {
-    image: '/img/ziverge.png',
+    image: '/theme/img/ziverge.png',
     imageAlt: 'Ziverge',
     imageLink: 'https://ziverge.com',
     content: 'Brilliant solutions for innovative companies'
   },
   {
-    image: `/img/scalac.svg`,
+    image: `/theme/img/scalac.svg`,
     imageAlt: 'Scalac',
     imageLink: 'https://scalac.io/',
     content: 'Scale fast with Scala'
   },
   {
-    image: `/img/septimal_mind.svg`,
+    image: `/theme/img/septimal_mind.svg`,
     imageAlt: 'Septimal Mind',
     imageLink: 'https://7mind.io/',
     content: 'Inventing productivity'
