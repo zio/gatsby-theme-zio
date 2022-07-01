6661c3cd88d7ba285865e773314125d96fd9bfb5
Andreas Gies <andreas@wayofquality.de>
2022-07-01 21:10:29 +0200

    Tailwind working for theme and sample

--- example/tailwind.config.js
+++ example/tailwind.config.js
@@ -1,9 +1,11 @@
 
 const colors = require('tailwindcss/colors')
 
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}", "./node_modules/@atooni/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", 
+    "./node_modules/@atooni/gatsby-theme-zio/src/**/*.{js,jsx,ts,tsx}"
+  ],
   theme: {
     extend: {
       colors: { 
