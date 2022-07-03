9e1cbd98a168238bef385428c3ee9491759ecbe7
Andreas Gies <andreas@wayofquality.de>
2022-07-03 11:35:34 +0200

    Some Prism logging

--- gatsby-theme-ziodoc/gatsby-browser.js
+++ gatsby-theme-ziodoc/gatsby-browser.js
@@ -1,11 +1,13 @@
 import * as React from "react"
+import * as Prism from "prismjs"
 import "./src/styles/tailwind.css"
 import "prism-themes/themes/prism-material-dark.css"
-
 import Root from "./src/components/root"
 
-export const wrapRootElement = ({element}) => {
+Prism.manual = true
+console.log(Prism.languages)
 
+export const wrapRootElement = ({element}) => {
   return(
     <Root>{element}</Root>
   )
