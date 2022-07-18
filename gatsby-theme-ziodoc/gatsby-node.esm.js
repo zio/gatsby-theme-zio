de0e7db5fc514f3ae6da315e87802a2047f8ef99
Andreas Gies <andreas@wayofquality.de>
2022-07-18 11:51:27 +0200

    Project configuration

--- gatsby-theme-ziodoc/gatsby-node.esm.js
+++ gatsby-theme-ziodoc/gatsby-node.esm.js
@@ -1,6 +1,9 @@
 import * as fs from "fs"
 import * as path from "path"
 import * as utils from "./src/utils"
+import * as menu from "./src/utils/menu"
+
+const fsp = fs.promises
 
 // Make sure the data directory exists.
 exports.onPreBootstrap = ({reporter}) => {
@@ -14,12 +17,65 @@ exports.onPreBootstrap = ({reporter}) => {
   }
 }
 
-export const createPages = ({ graphql, actions, reporter}) => {
+const listDirs = async (dir) => {
+  const res = await fsp.readdir(dir)
+  return res
+}
+
+export const createSchemaCustomization = ({ actions }) => {
+  const { createTypes } = actions 
+
+  const typeDefs = `
+    type ProjectYaml implements Node {
+      name: String!
+      version: String!
+      sourceInstance: String!
+    }
+  `
+
+  createTypes(typeDefs)
+}
+
+// get the project by src instance name
+function project(projects, inst) { 
+  return projects.find(p => p.sourceInstance === inst) 
+} 
+
+function buildMenus(mdxFiles, projects) { 
+  const root = new menu.Menu("root")
+
+  projects.forEach( p => {
+    root.addEntry([p.name, p.version])
+  })
+
+  mdxFiles.forEach( file => {
+    if (file.children.length >= 1) {
+      const prj = project(projects, file.sourceInstanceName)
+      const mdxChild = file.children[0]
+
+      var menuPath = []
+      if (prj) {
+        menuPath.push(prj.name)
+        menuPath.push(prj.version)
+      }
+
+      file.relativeDirectory.split("/").forEach(p => menuPath.push(p))
+      menuPath.push(file.name)
+
+      root.addEntry(menuPath)
+
+    }
+  })
+
+  return root
+}
+  
+export const createPages = ({ graphql, actions}) => {
  
   const { createPage } = actions
   const MdxComponent = path.resolve("./src/components/mdxcontent/MdxContent.js")
 
-  return new Promise( (resolve, reject) => {
+  return new Promise( (resolve, _) => {
     resolve(
       graphql(`
         {
@@ -34,6 +90,10 @@ export const createPages = ({ graphql, actions, reporter}) => {
               children {
                 ... on Mdx {
                   id
+                  frontmatter {
+                    title
+                    sidebar_label
+                  }
                   fileAbsolutePath
                 }
               }
@@ -42,16 +102,9 @@ export const createPages = ({ graphql, actions, reporter}) => {
           }
           allProjectYaml {
             nodes {
-              id
               name
               version
               sourceInstance
-            }
-          }
-          allSitePlugin(filter: {name: {eq: "gatsby-source-filesystem"}}) {
-            nodes {
-              pluginOptions
-              name
             }
           }
         }
@@ -60,32 +113,29 @@ export const createPages = ({ graphql, actions, reporter}) => {
   }).then( result => {
     // The array of all projects
     const projects = result.data.allProjectYaml.nodes
-    // From the file source plugins we just need the pluginOptions
-    const plugins = result.data.allSitePlugin.nodes.map(p => p.pluginOptions)
 
-    // extract the pluginOptions by src instance name
-    const src = inst => { return plugins.find(p => p.name === inst) }
-
-    // get the project by src instance name
-    const project = inst => { return projects.find(p => p.sourceInstance === inst) } 
-
+    // Just get the mdx files 
     const mdxFiles = result.data.allFile.nodes.filter(f => f.internal.mediaType === "text/markdown" || f.internal.mediaType === "text/mdx")
+
+    const menus = buildMenus(mdxFiles, projects)
+    console.log(mdxFiles)
 
     mdxFiles.forEach( file => {
       if (file.children.length >= 1) {
-        const prj = project(file.sourceInstanceName)
+        const prj = project(projects, file.sourceInstanceName)
         const mdxChild = file.children.shift()
 
         // in case we do have an index page we will serve that at the address of the directory name 
         const basePath = prj ? `${prj.name}/${prj.version}` : ''
-        const slug =  (utils.slugify(`${basePath}/${file.relativeDirectory}/${file.name}`)).replace(/\/index$/, '')
-
-        const source = src(file.sourceInstanceName)
-        reporter.info(`Creating page ${slug} -- ${basePath}`)
+        var slug =  (utils.slugify(`${basePath}/${file.relativeDirectory}/${file.name}`)).replace(/\/index$/, '/')
+        
+        console.log(`${mdxChild.fileAbsolutePath}`)
         createPage({
           path: slug,
           component: MdxComponent,
           context: {
+            menus: menus,
+            project: prj,
             filePath: mdxChild.fileAbsolutePath
           }
         })
